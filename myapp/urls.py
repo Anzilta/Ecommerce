6eee62f8f992725244c61e68885f99a9b010e015
Anzil <anzilasd@gmail.com>
2025-05-11 11:30:28 +0530

    Add today's updates

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns =[
    path("signup",views.register,name='signup'),
    path('',views.login_view,name="login"),
-   path('home/',views.home_view,name="home")
+   path('home/',views.home_view,name="home"),
+   path('addproduct/',views.add_product,name="add_product"),
+   path('adminhome/',views.admin_view,name="admin_home"),
+   path('editprofile/',views.edit_view,name="update_user"),
+   path('logout/', views.logout_view, name='logout'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .models import Userdetails
-from django.contrib.auth import login,authenticate
+from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.models import User
 
 
@@ -33,6 +33,7 @@ def login_view(request):
 
         email=request.POST.get("email")
         password=request.POST.get("password")
+    
         try:
             user_obj = Userdetails.objects.get(email=email)
             username = user_obj.username
@@ -50,3 +51,34 @@ def login_view(request):
     return render(request, './login.html')
 def home_view(request):
     return render(request,"./home.html")
+def add_product(request):
+    return render(request,"add_product.html")
+def admin_view(request):
+    return render(request,"admin_home.html")
+
+def edit_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+    user=request.user
+
+    if request.method=='POST':
+        username=request.POST.get('username')
+        place=request.POST.get('place')
+        age=request.POST.get('age')
+        phoneNumber=request.POST.get('phoneNumber')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+
+        user.username=username
+        user.place=place
+        user.age=age
+        user.phoneNumeber=phoneNumber
+        user.email=email
+        user.password=password
+        user.save()
+
+        return redirect("home")
+    return render(request,'update_user.html',{'user':user}) 
+def logout_view(request):
+    logout(request)
+    return redirect("login")   
